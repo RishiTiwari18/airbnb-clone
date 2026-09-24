c996da599deb64c1c2d4ede8cae04c4cad78ff98
Rishi Tiwari <rishiitiwarii18@gmail.com>
2026-09-25 00:23:04 +0530

    feat: initialize React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <h1>Welcome to Airbnb</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>Airbnb</h2>
+
+      <div>
+        <button>Homes</button>
+        <button>Experiences</button>
+        <button>Online Experiences</button>
+      </div>
+
+      <button>Login</button>
+    </nav>
+  );
+}
+
+export default Navbar;
